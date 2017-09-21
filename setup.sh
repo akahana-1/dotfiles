2ae6fa1f3eb96d4089e76097ba9b35c0d944763a
akahana-1 <h163341@edu.tut.ac.jp>
2017-09-21 23:10:38 +0900

    セットアッパーを改良中

--- setup.sh
+++ setup.sh
@@ -1,32 +1,34 @@
+#! /bin/bash
+
 if [ ! $XDG_CONFIG_HOME ]; then
-	HOME_DIR=$HOME
+	CONFIG_HOME=$HOME/.config
 else
-	HOME_DIR=$XDG_CONFIG_HOME
+	CONFIG_HOME=$XDG_CONFIG_HOME
 fi
 
-if [ -x "nvim" ]; then
-	EDITOR_TARGET="nvim"
-	EDITOR_CONFIG_DIR="${HOME_DIR}/.config/nvim"
+mkdir -p ${CONFIG_HOME}
 
-	mkdir -p "${HOME_DIR}/.config"
+if type "nvim" > /dev/null 2&>1; then
+	EDITOR_TARGET="nvim"
+	EDITOR_CONFIG_DIR="${CONFIG_HOME}/nvim"
 else
 	EDITOR_TARGET="vim"
 	EDITOR_CONFIG_DIR="${HOME_DIR}/.vim"
 fi
 
-ln -sf ".vim" ${EDITOR_CONFIG_DIR} 2> /dev/null
+ln -sf .vim ${EDITOR_CONFIG_DIR} 2> /dev/null
 
-for f in "dein.toml" "dein_lazy.toml"; do
+for f in "*.toml"; do
 	ln -sf ${f} ${EDITOR_CONFIG_DIR}/${f} 2> /dev/null
 done
 
-if "nvim" -eq ${EDITOR_TARGET}; then
+if [[ "nvim" == ${EDITOR_TARGET} ]]; then
 	ln -sf ".vimrc" "${EDITOR_CONFIG_DIR}/init.vim"
 else
 	ln -sf ".vimrc" "${HOME}/.vimrc"
 fi
 
-if ${SHELL##*/} -eq "bash"; then
+if [[ ${SHELL##*/} == "bash" ]]; then
 	if [ ! -f "${HOME}/.bashrc" ]; then
 		touch "${HOME}/.bashrc"
 	fi
